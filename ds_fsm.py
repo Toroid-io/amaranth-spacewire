7b1a28806adf3fd927fa423bf332849c6fee62c6
Andrés Manelli <andresmanelli@gmail.com>
2021-09-04 18:48:06 +0200

    Handle error paths in FSM. Improve tx and rx handling

--- ds_fsm.py
+++ ds_fsm.py
@@ -5,6 +5,7 @@ from bitarray import bitarray
 from ds_transmitter import DSTransmitter
 from ds_receiver import DSReceiver
 from ds_delay import DSDelay
+import math
 
 
 class DS_FSM(Elaboratable):
@@ -33,23 +34,19 @@ class DS_FSM(Elaboratable):
         m.submodules.rx = rx = DSReceiver()
         m.submodules.delay = delay = DSDelay(self._srcfreq, 12.8e-6, strategy='at_least')
 
-        tx_reset = Signal(reset=1)
-        tx_can_send_char = Signal()
-        tx_can_send_fct = Signal()
-        rx_reset = Signal(reset=1)
+        tr_pre_send = Signal()
+        rx_error = Signal()
         rx_tokens_count = Signal(range(2 + 1), reset=2) # TODO range rx fifo size / 8
         rx_read_counter = Signal(range(8))
         tx_credit = Signal(range(16 + 1)) # TODO tx fifo size
         read_in_progress = Signal()
 
         m.d.comb += [
-            tr.i_reset.eq(tx_reset),
             self.o_d.eq(tr.o_d),
             self.o_s.eq(tr.o_s),
-            rx.i_reset.eq(rx_reset),
             rx.i_d.eq(tr.o_d),
             rx.i_s.eq(tr.o_s),
-            tx_can_send_fct.eq((rx_tokens_count > 0) & ((tr.o_ready & ~tr.i_send_fct) == 1))
+            rx_error.eq(rx.o_escape_error | rx.o_parity_error | rx.o_read_error) # TODO: Missing disconnect error
         ]
 
         m.submodules.rx_fifo = rx_fifo = SyncFIFOBuffered(width=8, depth=16)
@@ -59,88 +56,113 @@ class DS_FSM(Elaboratable):
             rx_fifo.r_en.eq(self.i_r_en),
             self.o_r_data.eq(rx_fifo.r_data),
             self.o_r_rdy.eq(rx_fifo.r_rdy),
+            rx_fifo.w_data.eq(rx.o_data_char),
 
             tx_fifo.w_en.eq(self.i_w_en),
             tx_fifo.w_data.eq(self.i_w_data),
             self.o_w_rdy.eq(tx_fifo.w_rdy),
 
             tr.i_char.eq(tx_fifo.r_data),
-            tx_can_send_char.eq((tx_fifo.level > 0) & tr.o_ready & ~tx_fifo.r_en & (tx_credit > 0)),
             read_in_progress.eq(self.i_r_en & rx_fifo.r_rdy)
         ]
 
         m.d.sync += [
-            tr.i_send_char.eq(0),
-            tr.i_send_fct.eq(0),
             tr.i_send_eop.eq(0),
             tr.i_send_eep.eq(0),
-            tr.i_send_esc.eq(0),
-            tx_fifo.r_en.eq(0),
-            rx_fifo.w_en.eq(0)
+            tr.i_send_esc.eq(0)
         ]
 
         with m.FSM() as main_fsm:
             with m.State("ErrorReset"):
+                m.d.comb += rx.i_reset.eq(1)
+                m.d.comb += tr.i_reset.eq(1)
+
                 with m.If(delay.o_half_elapsed):
                     m.d.comb += delay.i_reset.eq(1)
                     m.next = "ErrorWait"
                 with m.Else():
                     m.d.comb += delay.i_start.eq(1)
-                    m.d.sync += rx_reset.eq(0)
             with m.State("ErrorWait"):
-                with m.If(delay.o_elapsed == 1):
+                m.d.comb += rx.i_reset.eq(0)
+                m.d.comb += tr.i_reset.eq(1)
+
+                with m.If((rx_error | rx.o_got_fct | rx.o_got_data) == 1): # TODO: Missing o_got_time_code
+                    m.d.comb += delay.i_reset.eq(1)
+                    m.next = "ErrorReset"
+                with m.Elif(delay.o_elapsed == 1):
+                    m.d.comb += delay.i_reset.eq(1)
                     m.next = "Ready"
                 with m.Else():
                     m.d.comb += delay.i_start.eq(1)
             with m.State("Ready"):
-                with m.If(self.i_link_enable == 1):
-                    m.d.sync += tx_reset.eq(0)
+                m.d.comb += rx.i_reset.eq(0)
+                m.d.comb += tr.i_reset.eq(1)
+
+                with m.If((rx_error | rx.o_got_fct | rx.o_got_data) == 1): # TODO: Missing o_got_time_code
+                    m.d.comb += delay.i_reset.eq(1)
+                    m.next = "ErrorReset"
+                with m.Elif(self.i_link_enable == 1):
                     m.next = "Started"
             with m.State("Started"):
-                with m.If(rx.o_got_null == 1):
+                m.d.comb += rx.i_reset.eq(0)
+                m.d.comb += tr.i_reset.eq(0)
+
+                with m.If((rx_error | rx.o_got_fct | rx.o_got_data | delay.o_elapsed) == 1): # TODO: Missing o_got_time_code
+                    m.d.comb += delay.i_reset.eq(1)
+                    m.next = "ErrorReset"
+                with m.Elif(rx.o_got_null == 1):
+                    m.d.comb += delay.i_reset.eq(1)
                     m.next = "Connecting"
+                with m.Else():
+                    m.d.comb += delay.i_start.eq(1)
             with m.State("Connecting"):
-                with m.If(tx_can_send_fct):
-                    m.d.sync += tr.i_send_fct.eq(1)
+                m.d.comb += rx.i_reset.eq(0)
+                m.d.comb += tr.i_reset.eq(0)
 
-                with m.If(rx.o_got_fct == 1):
+                with m.If((rx_error | rx.o_got_data | delay.o_elapsed) == 1): # TODO: Missing o_got_time_code
+                    m.d.comb += delay.i_reset.eq(1)
+                    m.next = "ErrorReset"
+                with m.Elif(rx.o_got_fct == 1):
                     m.d.sync += tx_credit.eq(tx_credit + 8)
+                    m.d.comb += delay.i_reset.eq(1)
                     m.next = "Run"
+                with m.Else():
+                    m.d.comb += delay.i_start.eq(1)
             with m.State("Run"):
-                # TX
-                with m.If(tx_can_send_fct):
-                    m.d.sync += tr.i_send_fct.eq(1)
-                with m.Elif(tx_can_send_char):
-                    m.d.sync += [
-                        tr.i_send_char.eq(1),
-                        tx_fifo.r_en.eq(1)
-                    ]
+                m.d.comb += rx.i_reset.eq(0)
+                m.d.comb += tr.i_reset.eq(0)
+                m.d.comb += tr_pre_send.eq((tx_fifo.level > 0) & tr.o_ready & (tx_credit > 0) & tx_fifo.r_rdy)
 
-                #RX
-                with m.If(rx.o_got_data & ~rx_fifo.w_en):
-                    m.d.sync += [
-                        rx_fifo.w_en.eq(1),
-                        rx_fifo.w_data.eq(rx.o_data_char)
-                    ]
+                with m.If((rx_error | ~self.i_link_enable) == 1): # TODO: Missing credit error
+                    m.d.comb += delay.i_reset.eq(1)
+                    # TODO Reset tokens and credits
+                    m.next = "ErrorReset"
+                with m.Else():
+                    # TX: i_send_char shifted a cycle to allow tx_fifo to output its value
+                    m.d.comb += tx_fifo.r_en.eq(tr_pre_send)
+                    m.d.comb += tr.i_send_char.eq(tr_pre_send)
+                    m.d.comb += rx_fifo.w_en.eq(rx.o_got_data)
 
         # Tokens and credit management
         with m.If(main_fsm.ongoing("Connecting") | main_fsm.ongoing("Run")):
-            with m.If(tx_can_send_char & ~rx.o_got_fct):
+            m.d.comb += tr.i_send_fct.eq((rx_tokens_count > 0) & (tr.o_ready == 1))
+
+            with m.If(tr.i_send_char & ~rx.o_got_fct):
                 m.d.sync += tx_credit.eq(tx_credit - 1)
-            with m.If(rx.o_got_fct & ~tx_can_send_char):
+            with m.If(rx.o_got_fct & ~tr.i_send_char):
                 m.d.sync += tx_credit.eq(tx_credit + 8)
-            with m.Elif(rx.o_got_fct & tx_can_send_char):
+            with m.Elif(rx.o_got_fct & tr.i_send_char):
                 m.d.sync += tx_credit.eq(tx_credit + 7)
 
             with m.If(read_in_progress):
                 m.d.sync += rx_read_counter.eq(rx_read_counter + 1)
 
-            with m.If(read_in_progress & ~tx_can_send_fct):
+            with m.If(read_in_progress & ~tr.i_send_fct):
                 with m.If(rx_read_counter == 7):
                     m.d.sync += rx_tokens_count.eq(rx_tokens_count + 1)
-            with m.Elif(~read_in_progress & tx_can_send_fct):
+            with m.Elif(~read_in_progress & tr.i_send_fct):
                 m.d.sync += rx_tokens_count.eq(rx_tokens_count - 1)
-            with m.If(read_in_progress & tx_can_send_fct):
+            with m.If(read_in_progress & tr.i_send_fct):
                 with m.If(~(rx_read_counter == 7)):
                     m.d.sync += rx_tokens_count.eq(rx_tokens_count - 1)
 
@@ -151,8 +173,8 @@ class DS_FSM(Elaboratable):
 
 
 if __name__ == '__main__':
-    srcfreq = 5e6
-    linkfreq = 0.25e6
+    srcfreq = 50e6
+    linkfreq = 45e6
     i_link_enable = Signal()
     m = Module()
     m.submodules.fsm = fsm = DS_FSM(srcfreq, linkfreq)
@@ -168,23 +190,25 @@ if __name__ == '__main__':
         ret.frombytes(c.encode())
         return ret
 
+    def period_to_ticks(p):
+        return math.ceil(p * srcfreq)
+
     def test():
-        #yield i_char.eq(ord('A'))
-        for _ in range(150):
+        for _ in range(period_to_ticks(25e-6)):
             yield
         yield i_link_enable.eq(1)
-        for _ in range(1200):
+        for _ in range(period_to_ticks(30e-6)):
             yield
-        for i in range(18):
+        for i in range(40):
             yield fsm.i_w_en.eq(1)
             yield fsm.i_w_data.eq(ord('A')+i)
             yield
         yield fsm.i_w_en.eq(0)
-        for _ in range(10000):
+        for _ in range(period_to_ticks(100e-6)):
             yield
 
     def read_from_fifo():
-        for _ in range(5000):
+        for _ in range(period_to_ticks(400e-6)):
             yield
         while True:
             if (yield fsm.o_r_rdy):
