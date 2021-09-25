1aef698886dd316b33bbfb06507e5f67dc5e5486
Andrés Manelli <andresmanelli@gmail.com>
2021-09-25 15:09:16 +0200

    Do not use Simulator to avoid warning

--- spw_node/test/signal_level/c.py
+++ spw_node/test/signal_level/c.py
@@ -1,5 +1,4 @@
 from nmigen import *
-from nmigen.sim import Simulator, Delay, Settle
 from spw_node.src.spw_transmitter import SpWTransmitter, WrongSignallingRate
 
 
@@ -13,21 +12,13 @@ def test_6_6_1():
         print(exc)
         assert(False)
 
+    dut1._MustUse__silence = True
+
     try:
         # Expect failure
         dut2 = SpWTransmitter(10e6, BIT_FREQ_MIN - 1)
         assert(False)
     except WrongSignallingRate as exc:
         print(exc)
-
-    sim = Simulator(dut1)
-
-    # Avoid unused warning
-    def dummy_test():
-        yield Delay(1e-6)
-
-    sim.add_process(dummy_test)
-
-    sim.run()
 
 tests = [test_6_6_1]
